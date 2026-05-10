949d0e5b37cfee3f9d719a7a6dd9b0232d2ec848
elahe yousefi <yousefyelahe@gmail.com>
2026-05-10 07:07:51 +0330

    remove unwanted files

--- target/classes/db/migration/V4__add_define_task.sql
+++
@@ -1,4 +0,0 @@
-insert into defined_task(id, period_day, start_date, task_name) values
-(1, 30, '2026/1/1', 'Vibration'),
-(2, 30, '2026/1/1', 'Energy Management'),
-(3, 30, '2026/1/1', 'Oil Analyze');
--- target/classes/db/migration/V7__add_employee.sql
+++
@@ -1,3 +0,0 @@
-insert into employee(id, name) values
-(1, 'Elahe Yousefi'),
-(2, 'Behrouz Keshavarz')
